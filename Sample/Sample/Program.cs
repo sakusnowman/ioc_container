e7d0bd6ac47d1e79e54a4015d6b7171a4b769f32
Yuki Sakumoto <cooking.of.the.magic.11@gmail.com>
2018-07-27 00:44:01 +0900

    Add Sample Project

---
+++ Sample/Sample/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sample.Repositories;
+using Sample.Views;
+using Sample.Services;
+using IocLabo;
+
+namespace Sample
+{
+    class Program
+    {
+        static CounterViewer viewer;
+        static void Main(string[] args)
+        {
+            new SetUp();
+            viewer = Labo.ConstructByLongestArgs<CounterViewer>();
+            Sequence();
+        }
+
+        static void Sequence()
+        {
+            while (true)
+            {
+                viewer.ShowAllCounters();
+                viewer.AcceptCommand();
+            }
+        }
+    }
+}
